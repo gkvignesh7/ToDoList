2fc3349769e71c201aff5c07c39ea9c7494d5e8c
gkvignesh-euni <vignesh.g@eunimart.com>
2023-01-04 19:25:52 +0530

    Fist commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header';
+// import Registration from './Components/Registration';
+
+
+const App=()=> {
+  return (
+    <div className="main-container">
+       <div className="sub-wrapper">
+          <div>
+          <Header/> 
+          </div>
+        </div>   
+
+     
+ 
+    </div>
+  );
+}
+
+export default App;
